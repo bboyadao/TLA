d5ea7544a2bf38ee400ae5ed629313b708c6915b
BBOYADAO <bboyadao@gmail.com>
2023-04-15 00:32:15 +0700

    add more bugs.

--- main.tf
+++ main.tf
@@ -50,33 +50,101 @@ resource "tls_private_key" "node_key" {
   rsa_bits  = 4096
 }
 
+resource "lxd_storage_pool" "k8s_storage_pool" {
+  name = "k8s_storage_pool"
+  driver = "zfs"
+  config = {
+    "zfs.pool_name" = "k8s_storage_pool"
+    source = "/dev/sda1"
+  }
+}
+
+
+resource "lxd_volume" "k8s_volumes" {
+  depends_on = [
+    lxd_storage_pool.k8s_storage_pool
+  ]
+  name = "k8s_volumes"
+  pool = "k8s_storage_pool"
+}
+
+resource "lxd_network" "k8s_network" {
+  name = "k8s_network"
+
+  config = {
+    "ipv4.address" = "10.10.1.1/24"
+    "ipv6.address" = ""
+    "ipv4.nat"      = "true"
+    "ipv6.nat"      = "true"
+    "ipv4.firewall" = "false"
+    "ipv6.firewall" : "false"
+
+  }
+
+#  provisioner "local-exec" {
+#    command     = "sudo firewall-cmd --zone=trusted --change-interface=${lxd_network.k8s_network.name} --permanent"
+#    interpreter = ["/usr/bin/bash", "-c"]
+#  }
+}
+
+resource "lxd_profile" "k8s_profile" {
+  depends_on = [
+    lxd_network.k8s_network,
+    lxd_volume.k8s_volumes
+  ]
+  name = "k8s"
+  description = "k8s profile"
+  config = {
+    "limits.cpu" = 4
+    "limits.memory" = "4GB"
+    "limits.memory.swap" = "false"
+    "linux.kernel_modules" = "ip_tables,ip6_tables,nf_nat,overlay,br_netfilter"
+    "raw.lxc" = <<EOF
+lxc.apparmor.profile=unconfined
+lxc.cap.drop=
+lxc.cgroup.devices.allow=a
+lxc.mount.auto=proc:rw sys:rw
+EOF
+    "security.privileged" = "true"
+    "security.nesting" = "true"
+  }
+
+  device {
+    name = "eth0"
+    type = "nic"
+
+    properties = {
+      name = "eth0"
+      network  = lxd_network.k8s_network.name
+    }
+  }
+
+  device {
+    name = "root"
+    type = "disk"
+
+    properties = {
+      pool = "k8s_storage_pool"
+      path = "/"
+    }
+  }
+}
+
+resource "lxd_cached_image" "debian11" {
+  source_remote = "images"
+  source_image  = "debian/11"
+}
 
 resource "lxd_container" "servers" {
   for_each = toset(local.instances)
   name      = each.value
-  image     = "images:debian/11"
+  image     = lxd_cached_image.debian11.fingerprint
   ephemeral = false
-  profiles  = ["default"]
-  config = {
-    "boot.autostart" = true
-    "security.nesting" = true
-    "security.syscalls.intercept.mknod" = true
-    "security.syscalls.intercept.setxattr" = true
-    "linux.kernel_modules" = "overlay, br_netfilter"
-  }
+  profiles  = [lxd_profile.k8s_profile.name]
 
-  limits = {
-    cpu = 2
-  }
-}
-output "instances" {
-  value = [for i, j in lxd_container.servers: "${i}: ${j.ip_address}"]
+
 }
 
-output "master_key" {
-  value = tls_private_key.master_key.private_key_pem
-  sensitive = true
-}
 
 resource "lxd_container_file" "files" {
   depends_on = [lxd_container.servers]
@@ -96,6 +164,7 @@ resource "null_resource" "provision" {
   for_each = lxd_container.servers
 
   connection {
+    timeout     = "10"
     type        = "ssh"
     user        = "root"
     host        = each.value.ip_address
@@ -118,8 +187,6 @@ resource "null_resource" "provision" {
     command = "lxc exec ${each.value.name} -- systemctl restart ssh"
   }
 
-
-
   provisioner "remote-exec" {
     inline = [
       "hostname",
@@ -137,4 +204,17 @@ resource "local_file" "hosts_cfg" {
     }
   )
   filename = "./ansible/inventory/hosts.cfg"
+}
+
+output "instances" {
+  value = [for i, j in lxd_container.servers: "${i}: ${j.ip_address}"]
+}
+
+output "master_key" {
+  value = tls_private_key.master_key.private_key_pem
+  sensitive = true
+}
+
+output "network" {
+  value = lxd_network.k8s_network.name
 }
